388b79d578f430317600df32e22571e05a70054d
Rinse <rinse12@tutanota.com>
2024-02-18 11:42:02 +0000

    playing around with libp2p in browser, still can't publish to kubo pubsub

--- browser-pubsub-p2p-publish-to-kubo.js
+++ browser-pubsub-p2p-publish-to-kubo.js
@@ -1,17 +1,23 @@
 import { createLibp2p } from "libp2p";
 import { gossipsub } from "@chainsafe/libp2p-gossipsub";
-import { webTransport } from "@libp2p/webtransport";
 import { mplex } from "@libp2p/mplex";
 import { yamux } from "@chainsafe/libp2p-yamux";
 import { noise } from "@chainsafe/libp2p-noise";
-import { toString as uint8ArrayToString } from "uint8arrays/to-string";
+import { webRTC, webRTCDirect } from "@libp2p/webrtc";
+import { webSockets } from "@libp2p/websockets";
+import { dcutr } from "@libp2p/dcutr";
+import { ping } from '@libp2p/ping'
+import { webTransport } from "@libp2p/webtransport";
+import {
+  toString as uint8ArrayToString,
+  fromString as uint8ArrayFromString,
+} from "uint8arrays";
 import { bootstrap } from "@libp2p/bootstrap";
 import { identify as identifyService } from "@libp2p/identify";
 import { kadDHT } from "@libp2p/kad-dht";
-import { webRTCDirect } from "@libp2p/webrtc";
 import { createEd25519PeerId } from "@libp2p/peer-id-factory";
-import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
-import { autoNAT } from '@libp2p/autonat'
+import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
+import { autoNAT } from "@libp2p/autonat";
 // From https://github.com/ipfs/helia/blob/main/packages/helia/src/utils/bootstrappers.ts
 const bootstrapConfig = {
   list: [
@@ -20,6 +26,9 @@ const bootstrapConfig = {
     "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
     "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
     "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+    "/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star",
+    "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
+    "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
   ],
 };
 
@@ -77,17 +86,23 @@ const logEvents = (nodeName, node) => {
 (async () => {
   try {
     const peerId = await createEd25519PeerId();
-
+    /** @type {import('libp2p').Libp2pOptions} */
     const browserNode = await createLibp2p({
       // can't listen using webtransport in libp2p js
       // addresses: {listen: []},
-
+      addresses: {
+        listen: ["/webrtc"],
+      },
       peerDiscovery: [bootstrap(bootstrapConfig)],
       peerId,
       transports: [
-        webTransport(),
+        circuitRelayTransport({
+          discoverRelays: 1,
+        }),
+        webRTC(),
         webRTCDirect(),
-        circuitRelayTransport(), // TODO: test this later, probably need to upgrade libp2p
+        webTransport(),
+        webSockets(),
       ],
       streamMuxers: [yamux(), mplex()],
       connectionEncryption: [noise()],
@@ -95,18 +110,21 @@ const logEvents = (nodeName, node) => {
         // not sure why needed, doesn't connect without it
         // denyDialMultiaddr: async () => false
       },
-      connectionManager: {
-        maxConnections: 10,
-        minConnections: 5,
-      },
+      // connectionManager: {
+      //   maxConnections: 10,
+      //   minConnections: 5,
+      // },
       services: {
+        dcutr: dcutr(),
         identify: identifyService(), // required for peer discovery of pubsub
-        dht: kadDHT({}), // p2p peer discovery
+        dht: kadDHT({ clientMode: true }), // p2p peer discovery
         pubsub: gossipsub({
           allowPublishToZeroPeers: true,
         }),
-        nat: autoNAT(),
+        autoNAT: autoNAT(),
+        ping: ping()
       },
+      
     });
     logEvents("browser-node", browserNode);
 
@@ -123,13 +141,29 @@ const logEvents = (nodeName, node) => {
         )} on topic ${evt.detail.topic}`
       );
     });
+
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for libp2p
     browserNode.services.pubsub.subscribe(topic);
 
     const res = await browserNode.services.pubsub.publish(
       topic,
       uint8ArrayFromString("hello from browser p2p")
     );
+
     log(`browser node published to topic (${topic}) and result is `, res);
+
+    setInterval(async () => {
+      try {
+        const res = await browserNode.services.pubsub.publish(
+          topic,
+          uint8ArrayFromString("hello from browser p2p")
+        );
+
+        log(`browser node published to topic (${topic}) and result is `, res);
+      } catch (e) {
+        console.error(e);
+      }
+    }, 5000);
   } catch (e) {
     log(e);
     log(e.stack);
